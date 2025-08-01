377e36c2e1514c9bde6ab9e810d03920f5555f7b
Giulio <giulio.trichilo.pay@gmail.com>
2025-08-01 17:26:03 +0100

    local helicone URLslocal helicone URLs

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,4 +6,6 @@ export const NVM_API_KEY = process.env.NVM_API_KEY!;
 export const NVM_ENVIRONMENT = process.env.NVM_ENVIRONMENT || "testing";
 export const AGENT_DID = process.env.AGENT_DID!;
 export const HELICONE_API_KEY = process.env.HELICONE_API_KEY!;
+export const HELICONE_BASE_LOGGING_URL = process.env.HELICONE_BASE_LOGGING_URL!;
+export const HELICONE_MANUAL_LOGGING_URL = process.env.HELICONE_MANUAL_LOGGING_URL!;
 export const IS_DUMMY = process.env.IS_DUMMY === "true";
